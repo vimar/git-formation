439c2324d02c1026e0b901e82da27780e0d52a2c
Olivier V <olvivier.vandewynckele@education.gouv.fr>
2016-12-06 16:37:48 +0100

    Ajout d'une nouvelle branche dérivée de dev

--- dgrh/menesr.php
+++ dgrh/menesr.php
@@ -1,5 +1,7 @@
 Interface MENESR{
 	public function __construct(){}
 	public function getFonctionnaire(){}
+
+	public function setHoraire(){}
 	public function deleteFonctionnaire(boolean fillon){}
 }
